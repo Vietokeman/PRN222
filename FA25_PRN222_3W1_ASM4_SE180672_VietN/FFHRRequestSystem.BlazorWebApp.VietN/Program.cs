4c0dd67d9767f683a0ed49efcca31eadebf04d36
Vietokeman <vietbmt19@gmail.com>
2025-12-11 22:29:16 +0700

    feat: blazor provip slot 10

--- FA25_PRN222_3W1_ASM4_SE180672_VietN/FFHRRequestSystem.BlazorWebApp.VietN/Program.cs
+++ FA25_PRN222_3W1_ASM4_SE180672_VietN/FFHRRequestSystem.BlazorWebApp.VietN/Program.cs
@@ -1,29 +1,53 @@
 ﻿using FFHRRequestSystem.BlazorWebApp.VietN.Components;
 using FFHRRequestSystem.Services.VietN;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add services
 builder.Services.AddScoped<TicketProcessingVietNService>();
 builder.Services.AddScoped<ProcessingTypeVietNService>();
+builder.Services.AddScoped<SystemUserAccountService>();
+
+// Add authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "BlazorAuth";
+        options.LoginPath = "/account/login";
+        options.AccessDeniedPath = "/account/forbidden";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        options.SlidingExpiration = true;
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+
+// Add HttpContext accessor
+builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
